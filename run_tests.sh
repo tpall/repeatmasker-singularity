245c0ab1347372ba4cd2dad76f0adf1bdea14d0b
Taavi Päll <tapa741@gmail.com>
2019-05-02 17:10:16 +0300

    no idea

--- run_tests.sh
+++ run_tests.sh
@@ -3,10 +3,13 @@
 export SINGULARITY_IMAGE="${SINGULARITY_IMAGE:-repeatmasker-singularity.simg}"
 echo "Using Singularity image: ${SINGULARITY_IMAGE}"
 
+set -x
+set -e
+
 # Run RM
-singularity run ${SINGULARITY_IMAGE} -v
-singularity exec ${SINGULARITY_IMAGE} /usr/local/bin/RepeatMasker test/seqs/small-1.fa
-singularity exec ${SINGULARITY_IMAGE}  /usr/local/bin/RepeatMasker test/seqs/small-2.fa
+singularity run --app RepeatMasker ${SINGULARITY_IMAGE} -v
+singularity exec -s /bin/bash ${SINGULARITY_IMAGE} /usr/local/bin/RepeatMasker test/seqs/small-1.fa
+singularity exec -s /bin/bash ${SINGULARITY_IMAGE}  /usr/local/bin/RepeatMasker test/seqs/small-2.fa
 
 { set +x; } 2>/dev/null
 echo "All tests passed!"
