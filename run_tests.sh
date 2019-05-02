c4ac0ee41ebc377af36a7c26bbec03baefc5c4aa
Taavi Päll <tapa741@gmail.com>
2019-05-02 10:09:03 +0300

    rm version

--- run_tests.sh
+++ run_tests.sh
@@ -13,11 +13,7 @@ set -e
 set -x
 
 # Verify RM version
-<<<<<<< HEAD
-if [ $(singularity exec ${SINGULARITY_IMAGE} RepeatMasker | head -n 1 | awk -F'-' '{print $2}') = $(version) ]
-=======
 if [ $(singularity exec ${SINGULARITY_IMAGE} -v) = $(version) ]
->>>>>>> 66f41f8299ec38628e7f92a681db37ba15ff28ff
 then
    echo "all right, version's correct!"
 else
