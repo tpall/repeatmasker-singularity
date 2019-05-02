5789e09857bd0f08b666acb5e8ed90ce6ff45184
Taavi Päll <tapa741@gmail.com>
2019-05-02 16:45:55 +0300

    tests

--- run_tests.sh
+++ run_tests.sh
@@ -3,15 +3,6 @@
 export SINGULARITY_IMAGE="${SINGULARITY_IMAGE:-repeatmasker-singularity.simg}"
 echo "Using Singularity image: ${SINGULARITY_IMAGE}"
 
-version () {
-  singularity inspect "${SINGULARITY_IMAGE}" | \
-    grep "RM_VERSION" | \
-    awk -F'"' '{print $4}'
-}
-
-set -e
-set -x
-
 # Run RM
 singularity run ${SINGULARITY_IMAGE} -v
 singularity exec ${SINGULARITY_IMAGE} /usr/local/bin/RepeatMasker test/seqs/small-1.fa
